2d290ab1e702cb8bd350d938d2d58562b5b2e2d0
Lucien Hsiao <lmhsiao@ucsd.edu>
2026-05-16 18:13:15 -0700

    map update
    add bike lanes

--- map.js
+++ map.js
@@ -4,7 +4,7 @@ mapboxgl.accessToken = "pk.eyJ1IjoibG1oc2lhbyIsImEiOiJjbXA5MWlmMXcwaXc1MzRwb2Zvd
 
 const map = new mapboxgl.Map({
   container: "map",
-  style: "mapbox://styles/mapbox/streets-v12",
+  style: "mapbox://styles/mapbox/outdoors-v12",
 
   center: [-71.09415, 42.36027],
 
@@ -12,3 +12,38 @@ const map = new mapboxgl.Map({
   minZoom: 5,
   maxZoom: 18,
 });
+
+map.on("load", async () => {
+  map.addSource("boston_route", {
+    type: "geojson",
+    data: "https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson",
+  });
+
+  map.addLayer({
+    id: "bike-lanes",
+    type: "line",
+    source: "boston_route",
+    paint: {
+        "line-color": "#178C3A",
+        "line-width": 4,
+        "line-opacity": 0.7,
+    },
+  });
+  
+  map.addSource("cambridge_route", {
+    type: "geojson",
+    data: "https://raw.githubusercontent.com/cambridgegis/cambridgegis_data/main/Recreation/Bike_Facilities/RECREATION_BikeFacilities.geojson",
+    });
+
+  map.addLayer({
+    id: "cambridge-bike-lanes",
+    type: "line",
+    source: "cambridge_route",
+    paint: {
+        "line-color": "#178C3A",
+        "line-width": 4,
+        "line-opacity": 0.7,
+    },
+  });
+});
+
