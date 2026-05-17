9f24c7b6e07e7f6a7e18326bb0e3a34e1343e700
Lucien Hsiao <lmhsiao@ucsd.edu>
2026-05-16 17:45:21 -0700

    add map

--- map.js
+++ map.js
@@ -1,3 +1,14 @@
 import mapboxgl from "https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm";
 
-console.log("Mapbox loaded:", mapboxgl);
+mapboxgl.accessToken = "pk.eyJ1IjoibG1oc2lhbyIsImEiOiJjbXA5MWlmMXcwaXc1MzRwb2ZvdXo5M3kxIn0.EFz3_BbbTbmA-_-Ga9ptUw";
+
+const map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v12",
+
+  center: [-71.09415, 42.36027],
+
+  zoom: 12,
+  minZoom: 5,
+  maxZoom: 18,
+});
